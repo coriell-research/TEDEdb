28ff65d5e927abeb3202c8da5c468aab777d59ee
jcalendo <jcalendo@protonmail.com>
2025-10-15 20:35:05 +0000

    updated filtering for low initial lib sizes

--- bioprojects/PRJNA687660/analysis.R
+++ bioprojects/PRJNA687660/analysis.R
@@ -166,10 +166,11 @@ cm <- makeContrasts(
   levels = design
 )
 
+# Fix filtering for low lib sizes
 y <- DGEList(counts = counts, samples = metadata)
 cutoff <- 10 / (min(y$samples$lib.size) / 1e6)
 before <- aveLogCPM(y)
-keep <- rowSums(cpm(y) > cutoff) >= (ncol(y) * 0.7)
+keep <- before > 2
 y <- y[keep,, keep.lib.sizes = FALSE]
 after <- aveLogCPM(y)
 
