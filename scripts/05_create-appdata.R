2972396e23ba6ac97df9ac3e98176072071fc618
Gennaro Calendo <jcalendo@protonmail.com>
2025-11-28 19:17:21 +0000

    update paths, log10 outliers

--- scripts/05_create-appdata.R
+++ scripts/05_create-appdata.R
@@ -4,7 +4,7 @@
 # This script will import all SE files from each experiment and extract the DE
 # results into a single SummarizedExperiment object used as the backend database
 # for the Shiny app. Metadata information, annotated manually in Excel, is read
-# in and aligned with columns of the imported DE data to create a finalized SE 
+# in and aligned with columns of the imported DE data to create a finalized SE
 # object.
 #
 # ----------------------------------------------------------------------------
@@ -31,19 +31,17 @@ message("Found ", length(se_files), " results.")
 
 # Extract DE results from SE objects and bind into single DT -----------------
 
-
 message("Extracting DE data from SummarizedExperiments...")
 de <- rbindlist(
-  parallel::mclapply(se_files, extract_de, mc.cores = N_CORES), 
+  parallel::mclapply(se_files, extract_de, mc.cores = N_CORES),
   idcol = "BioProject"
-  )
+)
 
 # Add new column as unique identifier "id"
 de[, id := str_c(BioProject, contrast, sep = ".")]
 
 
 # Read in contrast-level metadata files --------------------------------------
-
 
 message("Reading in contrast-level metadata...")
 metafile <- here("appdata", "contrast-metadata - metadata.tsv")
@@ -65,15 +63,27 @@ missing_annotation <- setdiff(analyzed_ids, annotated_ids)
 missing_data <- setdiff(annotated_ids, analyzed_ids)
 
 if (length(missing_annotation) > 0) {
-  message("There are ", length(missing_annotation), " IDs without an annotation!")
+  message(
+    "There are ",
+    length(missing_annotation),
+    " IDs without an annotation!"
+  )
   message("Writing these IDs to: appdata/missing-annotations.tsv")
-  fwrite(data.table(id = missing_annotation), here("appdata", "missing-annotations.tsv"), sep = "\t")
+  fwrite(
+    data.table(id = missing_annotation),
+    here("appdata", "missing-annotations.tsv"),
+    sep = "\t"
+  )
 }
 
 if (length(missing_data) > 0) {
   message("There are ", length(missing_data), " IDs annotated without data!")
   message("Writing these IDs to: appdata/missing-data.tsv")
-  fwrite(data.table(id = missing_data), here("appdata", "missing-data.tsv"), sep = "\t")
+  fwrite(
+    data.table(id = missing_data),
+    here("appdata", "missing-data.tsv"),
+    sep = "\t"
+  )
 }
 
 message("Inner joining metadata onto differential expression data...")
@@ -82,37 +92,47 @@ metadata <- metadata[cells, on = "cell_line", nomatch = 0L]
 
 message("Flagging contrasts with potential outliers...")
 outliers <- getOutlierContrasts(se_files, cores = N_CORES)
-metadata[, `:=`(outlier_avg_frag_length_mean = id %chin% outliers$outlier_avg_frag_length_mean,
-                outlier_avg_frag_length_sd = id %chin% outliers$outlier_avg_frag_length_sd,
-                outlier_avg_num_eq_classes = id %chin% outliers$outlier_avg_num_eq_classes,
-                outlier_avg_num_processed = id %chin% outliers$outlier_avg_num_processed,
-                outlier_avg_num_mapped = id %chin% outliers$outlier_avg_num_mapped,
-                outlier_avg_num_decoy_fragments = id %chin% outliers$outlier_avg_num_decoy_fragments,
-                outlier_avg_num_dovetail_fragments = id %chin% outliers$outlier_avg_num_dovetail_fragments,
-                outlier_avg_num_fragments_filtered_vm = id %chin% outliers$outlier_avg_num_fragments_filtered_vm,
-                outlier_avg_num_alignments_below_threshold_vm = id %chin% outliers$outlier_avg_num_alignments_below_threshold_vm,
-                outlier_avg_percent_mapped = id %chin% outliers$outlier_avg_percent_mapped)]
+metadata[, `:=`(
+  outlier_avg_frag_length_mean = id %chin%
+    outliers$outlier_avg_frag_length_mean,
+  outlier_avg_frag_length_sd = id %chin% outliers$outlier_avg_frag_length_sd,
+  outlier_avg_num_eq_classes = id %chin% outliers$outlier_avg_num_eq_classes,
+  outlier_avg_num_processed = id %chin% outliers$outlier_avg_num_processed,
+  outlier_avg_num_mapped = id %chin% outliers$outlier_avg_num_mapped,
+  outlier_avg_num_decoy_fragments = id %chin%
+    outliers$outlier_avg_num_decoy_fragments,
+  outlier_avg_num_dovetail_fragments = id %chin%
+    outliers$outlier_avg_num_dovetail_fragments,
+  outlier_avg_num_fragments_filtered_vm = id %chin%
+    outliers$outlier_avg_num_fragments_filtered_vm,
+  outlier_avg_num_alignments_below_threshold_vm = id %chin%
+    outliers$outlier_avg_num_alignments_below_threshold_vm,
+  outlier_avg_percent_mapped = id %chin% outliers$outlier_avg_percent_mapped
+)]
 
 # Create a single catch-all flag
-metadata[, outlier_flags := getOutlierFlags(.SD), .SDcols = names(metadata) %like% "outlier"]
+metadata[,
+  outlier_flags := getOutlierFlags(.SD),
+  .SDcols = names(metadata) %like% "outlier"
+]
 metadata[outlier_flags == "", outlier_flags := "None"]
 
 # Remove the intermediate columns
-metadata[, `:=`(outlier_avg_frag_length_mean=NULL,
-                outlier_avg_frag_length_sd=NULL,
-                outlier_avg_num_eq_classes=NULL,
-                outlier_avg_num_processed=NULL,
-                outlier_avg_num_mapped=NULL,
-                outlier_avg_num_decoy_fragments=NULL,
-                outlier_avg_num_dovetail_fragments=NULL,
-                outlier_avg_num_fragments_filtered_vm=NULL,
-                outlier_avg_num_alignments_below_threshold_vm=NULL,
-                outlier_avg_percent_mapped=NULL
-              )]
+metadata[, `:=`(
+  outlier_avg_frag_length_mean = NULL,
+  outlier_avg_frag_length_sd = NULL,
+  outlier_avg_num_eq_classes = NULL,
+  outlier_avg_num_processed = NULL,
+  outlier_avg_num_mapped = NULL,
+  outlier_avg_num_decoy_fragments = NULL,
+  outlier_avg_num_dovetail_fragments = NULL,
+  outlier_avg_num_fragments_filtered_vm = NULL,
+  outlier_avg_num_alignments_below_threshold_vm = NULL,
+  outlier_avg_percent_mapped = NULL
+)]
 
 
 # Shape into matrices --------------------------------------------------------
-
 
 message("Creating assay data from differential expression results...")
 assay_cols <- c("logFC", "AveExpr", "z", "P.Value", "adj.P.Val", "SE")
@@ -120,7 +140,12 @@ assays <- vector("list", length(assay_cols))
 
 names(assays) <- assay_cols
 for (i in seq_along(assay_cols)) {
-  assays[[i]] <- col2assay(de, rows = "feature_id", cols = "id", vals = assay_cols[[i]])
+  assays[[i]] <- col2assay(
+    de,
+    rows = "feature_id",
+    cols = "id",
+    vals = assay_cols[[i]]
+  )
 }
 
 # Impute missing values for assays used in dim reduction
@@ -131,61 +156,83 @@ assays[["P.Value"]][is.na(assays[["P.Value"]])] <- 1
 
 # Create SummarizedExperiment object of DE results ---------------------------
 
-
 setDF(metadata, rownames = metadata$id)
 keep <- intersect(colnames(assays[[1]]), rownames(metadata))
 metadata <- metadata[keep, ]
 assays <- lapply(assays, \(x) x[, keep])
-stopifnot("rownames of metadata do not match colnames of matrices!" = all(colnames(assays[[1]]) == rownames(metadata)))
+stopifnot(
+  "rownames of metadata do not match colnames of matrices!" = all(
+    colnames(assays[[1]]) == rownames(metadata)
+  )
+)
 
-message("Creating final SummarizedExperiment object from differential expression results...")
+message(
+  "Creating final SummarizedExperiment object from differential expression results..."
+)
 se <- SummarizedExperiment(assays = assays, colData = metadata)
 
 
 # Create rowData -------------------------------------------------------------
 
-
 # TODO: This hardcoded path needs a permanent home in a top-level directory
 #  possibly add the analysis/ directory to the github repo as well
-gene_lengths <- fread("/home/gennaro/data/cancer-rnaseq-database/analysis/results/data-files/01/v26-gene-length-annotation.tsv")
+gene_lengths <- fread(
+  "/home/gennaro/data/TEDEdb/analysis/results/data-files/01/v26-gene-length-annotation.tsv"
+)
 gene_lengths <- gene_lengths[gene_type == "protein_coding"]
 
 rd <- data.table(feature_name = rownames(se))
-rd <- merge(x = rd, y = gene_lengths, by.x = "feature_name", by.y="gene_name", all.x=TRUE)
+rd <- merge(
+  x = rd,
+  y = gene_lengths,
+  by.x = "feature_name",
+  by.y = "gene_name",
+  all.x = TRUE
+)
 
 # Some gene names are replicated and need to be collapsed (mostly double annotated on PAR Y)
 rd <- rd[!gene_id %like% "PAR_Y"]
 
 # Select the longest of the gene_ids to keep
-multi <- rd[, .N, by=feature_name][N > 1][, feature_name]
+multi <- rd[, .N, by = feature_name][N > 1][, feature_name]
 all_ids <- rd[feature_name %chin% multi, unique(gene_id)]
 keep_ids <- rd[feature_name %chin% multi][
-               order(feature_name, -total_gene_length)][, 
-               head(.SD, n=1), by=feature_name][, gene_id]
+  order(feature_name, -total_gene_length)
+][,
+  head(.SD, n = 1),
+  by = feature_name
+][, gene_id]
 drop_ids <- setdiff(all_ids, keep_ids)
 rd <- rd[!gene_id %chin% drop_ids]
 
 # Add feature type labels and separate TE information
 rd[, feature_type := fifelse(feature_name %like% ".*\\..*\\..*", "TE", "Gene")]
-rd[feature_type == "TE", c("class", "family", "subfamily") := tstrsplit(feature_name, "\\.")]
+rd[
+  feature_type == "TE",
+  c("class", "family", "subfamily") := tstrsplit(feature_name, "\\.")
+]
 
 setDF(rd, rownames = rd$feature_name)
 rd$feature_name <- NULL
 
 rd <- rd[rownames(se), ]
-stopifnot("rownames(rowData) != rownames(se)" = all(rownames(rd) == rownames(se)))
+stopifnot(
+  "rownames(rowData) != rownames(se)" = all(rownames(rd) == rownames(se))
+)
 rowData(se) <- cbind(rowData(se), rd)
 
 
 # Save finalized object ------------------------------------------------------
 
-
 message("Saving HDF5-backed SE for database backend...")
-saveHDF5SummarizedExperiment(se, dir=here("appdata", "se_hdf5"), replace=TRUE)
+saveHDF5SummarizedExperiment(
+  se,
+  dir = here("appdata", "se_hdf5"),
+  replace = TRUE
+)
 
 
 # Select input data ----------------------------------------------------------
-
 
 # Create data used by the shiny app for populating select inputs
 select_inputs <- lapply(metadata, \(x) sort(unique(x)))
--- scripts/helpers.R
+++ scripts/helpers.R
@@ -13,7 +13,7 @@ col2assay <- function(df, rows, cols, vals) {
 getSampleMetadata <- function(x) {
   se <- readRDS(x)
   result <- data.table::as.data.table(data.frame(colData(se)))
-  
+
   return(result)
 }
 
@@ -24,73 +24,123 @@ getContrasts <- function(x) {
   cm <- data.table::as.data.table(cm, keep.rownames = "group")
 
   cm.m <- data.table::melt(
-      cm, 
-      id.vars="group", 
-      variable.name="contrast", 
-      value.name="is_member",
-      variable.factor=FALSE
-      )
+    cm,
+    id.vars = "group",
+    variable.name = "contrast",
+    value.name = "is_member",
+    variable.factor = FALSE
+  )
 
   return(cm.m)
 }
 
 
 getOutlierContrasts <- function(sefiles, cores) {
-
   # Sample-level metadata with Salmon meta_info
   message("Reading in sample-level metadata...")
   metadata <- data.table::rbindlist(
-      parallel::mclapply(sefiles, getSampleMetadata, mc.cores=cores), 
-      fill = TRUE,
-      idcol = "BioProject"
-      )
-  metadata[, group_id := paste(BioProject, group, sep=".")]
-  keep <- c("BioProject", "BioSample", "group", "group_id", "salmon_version", "library_types",
-            "avg_frag_length_mean", "avg_frag_length_sd", "avg_num_eq_classes", 
-            "avg_num_processed", "avg_num_mapped", "avg_num_decoy_fragments", 
-            "avg_num_dovetail_fragments", "avg_num_fragments_filtered_vm", 
-            "avg_num_alignments_below_threshold_vm", "avg_percent_mapped")
+    parallel::mclapply(sefiles, getSampleMetadata, mc.cores = cores),
+    fill = TRUE,
+    idcol = "BioProject"
+  )
+  metadata[, group_id := paste(BioProject, group, sep = ".")]
+  keep <- c(
+    "BioProject",
+    "BioSample",
+    "group",
+    "group_id",
+    "salmon_version",
+    "library_types",
+    "avg_frag_length_mean",
+    "avg_frag_length_sd",
+    "avg_num_eq_classes",
+    "avg_num_processed",
+    "avg_num_mapped",
+    "avg_num_decoy_fragments",
+    "avg_num_dovetail_fragments",
+    "avg_num_fragments_filtered_vm",
+    "avg_num_alignments_below_threshold_vm",
+    "avg_percent_mapped"
+  )
   metadata <- metadata[, ..keep]
-  metadata[, library_layout := data.table::fifelse(library_types %like% "^[IOM]", "PAIRED", "SINGLE")]
+  metadata[,
+    library_layout := data.table::fifelse(
+      library_types %like% "^[IOM]",
+      "PAIRED",
+      "SINGLE"
+    )
+  ]
 
   # Compute sample-level failures based on global information
   message("Computing outliers based on global distributions...")
-  metadata[, `:=`(is_outlier_avg_frag_length_mean  = coriell::outliers_by_mad(avg_frag_length_mean),                                
-                  is_outlier_avg_frag_length_sd  = coriell::outliers_by_mad(avg_frag_length_sd),                                 
-                  is_outlier_avg_num_eq_classes  = coriell::outliers_by_mad(avg_num_eq_classes, direction="low"),                               
-                  is_outlier_avg_num_processed  = coriell::outliers_by_mad(avg_num_processed, direction="low"),                                
-                  is_outlier_avg_num_mapped  = coriell::outliers_by_mad(avg_num_mapped, direction="low"),                                      
-                  is_outlier_avg_num_decoy_fragments  = coriell::outliers_by_mad(avg_num_decoy_fragments, direction="high"),                     
-                  is_outlier_avg_num_dovetail_fragments  = coriell::outliers_by_mad(avg_num_dovetail_fragments, direction="high"),                  
-                  is_outlier_avg_num_fragments_filtered_vm  = coriell::outliers_by_mad(avg_num_fragments_filtered_vm, direction="high"),             
-                  is_outlier_avg_num_alignments_below_threshold_vm  = coriell::outliers_by_mad(avg_num_alignments_below_threshold_vm, direction="high"),
-                  is_outlier_avg_percent_mapped  = coriell::outliers_by_mad(avg_percent_mapped, direction="low")
+  # fmt: skip
+  metadata[, `:=`(is_outlier_avg_frag_length_mean = coriell::outliers_by_mad(avg_frag_length_mean),                                
+                  is_outlier_avg_frag_length_sd = coriell::outliers_by_mad(avg_frag_length_sd),                                 
+                  is_outlier_avg_num_eq_classes = coriell::outliers_by_mad(log10(avg_num_eq_classes+1), direction="low"),                               
+                  is_outlier_avg_num_processed = coriell::outliers_by_mad(log10(avg_num_processed+1), direction="low"),                                
+                  is_outlier_avg_num_mapped = coriell::outliers_by_mad(log10(avg_num_mapped+1), direction="low"),                                      
+                  is_outlier_avg_num_decoy_fragments = coriell::outliers_by_mad(log10(avg_num_decoy_fragments+1), direction="high"),                     
+                  is_outlier_avg_num_dovetail_fragments = coriell::outliers_by_mad(log10(avg_num_dovetail_fragments+1), direction="high"),                  
+                  is_outlier_avg_num_fragments_filtered_vm = coriell::outliers_by_mad(log10(avg_num_fragments_filtered_vm+1), direction="high"),             
+                  is_outlier_avg_num_alignments_below_threshold_vm = coriell::outliers_by_mad(log10(avg_num_alignments_below_threshold_vm+1), direction="high"),
+                  is_outlier_avg_percent_mapped = coriell::outliers_by_mad(avg_percent_mapped, direction="low")
                   ), by = library_layout]
 
   # Get contrast to group mapping
   message("Reading in group to contrast mapping...")
   cm_dt <- data.table::rbindlist(
-      parallel::mclapply(sefiles, getContrasts, mc.cores=cores), 
-      idcol="BioProject"
-      )
+    parallel::mclapply(sefiles, getContrasts, mc.cores = cores),
+    idcol = "BioProject"
+  )
   cm_dt <- cm_dt[is_member != 0]
-  cm_dt[, id := paste(BioProject, contrast, sep=".")]
-  cm_dt[, group_id := paste(BioProject, group, sep=".")]
+  cm_dt[, id := paste(BioProject, contrast, sep = ".")]
+  cm_dt[, group_id := paste(BioProject, group, sep = ".")]
 
   # Collect groups with samples that failed QC for some reason
   message("Determining groups with outlier samples...")
-  has_outlier_avg_frag_length_mean <- metadata[is_outlier_avg_frag_length_mean == TRUE, unique(group_id)]
-  has_outlier_avg_frag_length_sd <- metadata[is_outlier_avg_frag_length_sd == TRUE, unique(group_id)]
-  has_outlier_avg_num_eq_classes <- metadata[is_outlier_avg_num_eq_classes == TRUE, unique(group_id)]
-  has_outlier_avg_num_processed <- metadata[is_outlier_avg_num_processed == TRUE, unique(group_id)]
-  has_outlier_avg_num_mapped <- metadata[is_outlier_avg_num_mapped == TRUE, unique(group_id)]
-  has_outlier_avg_num_decoy_fragments <- metadata[is_outlier_avg_num_decoy_fragments == TRUE, unique(group_id)]
-  has_outlier_avg_num_dovetail_fragments <- metadata[is_outlier_avg_num_dovetail_fragments == TRUE, unique(group_id)]
-  has_outlier_avg_num_fragments_filtered_vm <- metadata[is_outlier_avg_num_fragments_filtered_vm == TRUE, unique(group_id)]
-  has_outlier_avg_num_alignments_below_threshold_vm <- metadata[is_outlier_avg_num_alignments_below_threshold_vm == TRUE, unique(group_id)]
-  has_outlier_avg_percent_mapped <- metadata[is_outlier_avg_percent_mapped == TRUE, unique(group_id)]
+  has_outlier_avg_frag_length_mean <- metadata[
+    is_outlier_avg_frag_length_mean == TRUE,
+    unique(group_id)
+  ]
+  has_outlier_avg_frag_length_sd <- metadata[
+    is_outlier_avg_frag_length_sd == TRUE,
+    unique(group_id)
+  ]
+  has_outlier_avg_num_eq_classes <- metadata[
+    is_outlier_avg_num_eq_classes == TRUE,
+    unique(group_id)
+  ]
+  has_outlier_avg_num_processed <- metadata[
+    is_outlier_avg_num_processed == TRUE,
+    unique(group_id)
+  ]
+  has_outlier_avg_num_mapped <- metadata[
+    is_outlier_avg_num_mapped == TRUE,
+    unique(group_id)
+  ]
+  has_outlier_avg_num_decoy_fragments <- metadata[
+    is_outlier_avg_num_decoy_fragments == TRUE,
+    unique(group_id)
+  ]
+  has_outlier_avg_num_dovetail_fragments <- metadata[
+    is_outlier_avg_num_dovetail_fragments == TRUE,
+    unique(group_id)
+  ]
+  has_outlier_avg_num_fragments_filtered_vm <- metadata[
+    is_outlier_avg_num_fragments_filtered_vm == TRUE,
+    unique(group_id)
+  ]
+  has_outlier_avg_num_alignments_below_threshold_vm <- metadata[
+    is_outlier_avg_num_alignments_below_threshold_vm == TRUE,
+    unique(group_id)
+  ]
+  has_outlier_avg_percent_mapped <- metadata[
+    is_outlier_avg_percent_mapped == TRUE,
+    unique(group_id)
+  ]
 
   # Add indicators for each group with sample failures
+  # fmt: skip
   cm_dt[, `:=`(outlier_avg_frag_length_mean = group_id %in% has_outlier_avg_frag_length_mean,
                outlier_avg_frag_length_sd = group_id %in% has_outlier_avg_frag_length_sd,
                outlier_avg_num_eq_classes = group_id %in% has_outlier_avg_num_eq_classes,
@@ -104,39 +154,71 @@ getOutlierContrasts <- function(sefiles, cores) {
 
   # Now collect the contrasts with the potential outliers
   message("Determining contrasts with outlier groups...")
-  con_outlier_avg_frag_length_mean <- cm_dt[outlier_avg_frag_length_mean == TRUE, unique(id)]
-  con_outlier_avg_frag_length_sd <- cm_dt[outlier_avg_frag_length_sd == TRUE, unique(id)]
-  con_outlier_avg_num_eq_classes <- cm_dt[outlier_avg_num_eq_classes == TRUE, unique(id)]
-  con_outlier_avg_num_processed <- cm_dt[outlier_avg_num_processed == TRUE, unique(id)]
-  con_outlier_avg_num_mapped <- cm_dt[outlier_avg_num_mapped == TRUE, unique(id)]
-  con_outlier_avg_num_decoy_fragments <- cm_dt[outlier_avg_num_decoy_fragments == TRUE, unique(id)]
-  con_outlier_avg_num_dovetail_fragments <- cm_dt[outlier_avg_num_dovetail_fragments == TRUE, unique(id)]
-  con_outlier_avg_num_fragments_filtered_vm <- cm_dt[outlier_avg_num_fragments_filtered_vm == TRUE, unique(id)]
-  con_outlier_avg_num_alignments_below_threshold_vm <- cm_dt[outlier_avg_num_alignments_below_threshold_vm == TRUE, unique(id)]
-  con_outlier_avg_percent_mapped <- cm_dt[outlier_avg_percent_mapped == TRUE, unique(id)]
+  con_outlier_avg_frag_length_mean <- cm_dt[
+    outlier_avg_frag_length_mean == TRUE,
+    unique(id)
+  ]
+  con_outlier_avg_frag_length_sd <- cm_dt[
+    outlier_avg_frag_length_sd == TRUE,
+    unique(id)
+  ]
+  con_outlier_avg_num_eq_classes <- cm_dt[
+    outlier_avg_num_eq_classes == TRUE,
+    unique(id)
+  ]
+  con_outlier_avg_num_processed <- cm_dt[
+    outlier_avg_num_processed == TRUE,
+    unique(id)
+  ]
+  con_outlier_avg_num_mapped <- cm_dt[
+    outlier_avg_num_mapped == TRUE,
+    unique(id)
+  ]
+  con_outlier_avg_num_decoy_fragments <- cm_dt[
+    outlier_avg_num_decoy_fragments == TRUE,
+    unique(id)
+  ]
+  con_outlier_avg_num_dovetail_fragments <- cm_dt[
+    outlier_avg_num_dovetail_fragments == TRUE,
+    unique(id)
+  ]
+  con_outlier_avg_num_fragments_filtered_vm <- cm_dt[
+    outlier_avg_num_fragments_filtered_vm == TRUE,
+    unique(id)
+  ]
+  con_outlier_avg_num_alignments_below_threshold_vm <- cm_dt[
+    outlier_avg_num_alignments_below_threshold_vm == TRUE,
+    unique(id)
+  ]
+  con_outlier_avg_percent_mapped <- cm_dt[
+    outlier_avg_percent_mapped == TRUE,
+    unique(id)
+  ]
 
   # Return a list of these IDs for use in the main annotation script
-  result <- list(outlier_avg_frag_length_mean = con_outlier_avg_frag_length_mean,
-                 outlier_avg_frag_length_sd = con_outlier_avg_frag_length_sd,
-                 outlier_avg_num_eq_classes = con_outlier_avg_num_eq_classes,
-                 outlier_avg_num_processed = con_outlier_avg_num_processed,
-                 outlier_avg_num_mapped = con_outlier_avg_num_mapped,
-                 outlier_avg_num_decoy_fragments = con_outlier_avg_num_decoy_fragments,
-                 outlier_avg_num_dovetail_fragments = con_outlier_avg_num_dovetail_fragments,
-                 outlier_avg_num_fragments_filtered_vm = con_outlier_avg_num_fragments_filtered_vm,
-                 outlier_avg_num_alignments_below_threshold_vm = con_outlier_avg_num_alignments_below_threshold_vm,
-                 outlier_avg_percent_mapped = con_outlier_avg_percent_mapped)
+  result <- list(
+    outlier_avg_frag_length_mean = con_outlier_avg_frag_length_mean,
+    outlier_avg_frag_length_sd = con_outlier_avg_frag_length_sd,
+    outlier_avg_num_eq_classes = con_outlier_avg_num_eq_classes,
+    outlier_avg_num_processed = con_outlier_avg_num_processed,
+    outlier_avg_num_mapped = con_outlier_avg_num_mapped,
+    outlier_avg_num_decoy_fragments = con_outlier_avg_num_decoy_fragments,
+    outlier_avg_num_dovetail_fragments = con_outlier_avg_num_dovetail_fragments,
+    outlier_avg_num_fragments_filtered_vm = con_outlier_avg_num_fragments_filtered_vm,
+    outlier_avg_num_alignments_below_threshold_vm = con_outlier_avg_num_alignments_below_threshold_vm,
+    outlier_avg_percent_mapped = con_outlier_avg_percent_mapped
+  )
 
   return(result)
 }
 
 
 getOutlierFlags <- function(l, sep = ",") {
-  mat <- do.call(cbind, l)             
-  result <- apply(mat, 1, function(row) {        
-    cols <- names(l)[which(row)] 
+  mat <- do.call(cbind, l)
+  result <- apply(mat, 1, function(row) {
+    cols <- names(l)[which(row)]
     paste(cols, collapse = sep)
   })
-  
+
   return(result)
 }
